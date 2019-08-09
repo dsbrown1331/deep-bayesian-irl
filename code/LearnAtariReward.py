deaeb174d35bc960274c3a12c879895b12b1f0a3
dsbrown1331 <dsbrown1331@gmail.com>
2019-08-08 22:43:48 -0400

    random search framework started

--- code/LearnAtariReward.py
+++ code/LearnAtariReward.py
@@ -19,9 +19,9 @@ from baselines.common.trex_utils import preprocess
 
 
 def generate_novice_demos(env, env_name, agent, model_dir):
-    checkpoint_min = 50
+    checkpoint_min = 100#50
     checkpoint_max = 600
-    checkpoint_step = 50
+    checkpoint_step = 100#50
     checkpoints = []
     if env_name == "enduro":
         checkpoint_min = 3100
@@ -67,7 +67,7 @@ def generate_novice_demos(env, env_name, agent, model_dir):
                 action = agent.act(ob, r, done)
                 ob, r, done, _ = env.step(action)
                 ob_processed = preprocess(ob, env_name)
-                ob_processed = ob_processed[0] #get rid of first dimension ob.shape = (1,84,84,4)
+                #ob_processed = ob_processed[0] #get rid of first dimension ob.shape = (1,84,84,4)
                 traj.append(ob_processed)
 
                 gt_rewards.append(r[0])
@@ -86,78 +86,23 @@ def generate_novice_demos(env, env_name, agent, model_dir):
 
 
 
-
-
-
-
-def create_training_data(demonstrations, num_trajs, num_snippets, min_snippet_length, max_snippet_length):
-    #collect training data
-    max_traj_length = 0
+def create_training_data(demonstrations):
+    '''create all pairwise rankings given list of sorted demonstrations'''
     training_obs = []
     training_labels = []
     num_demos = len(demonstrations)
-
-    #add full trajs (for use on Enduro)
-    for n in range(num_trajs):
-        ti = 0
-        tj = 0
-        #only add trajectories that are different returns
-        while(ti == tj):
-            #pick two random demonstrations
-            ti = np.random.randint(num_demos)
-            tj = np.random.randint(num_demos)
-        #create random partial trajs by finding random start frame and random skip frame
-        si = np.random.randint(6)
-        sj = np.random.randint(6)
-        step = np.random.randint(3,7)
-
-        traj_i = demonstrations[ti][si::step]  #slice(start,stop,step)
-        traj_j = demonstrations[tj][sj::step]
-
-        if ti > tj:
-            label = 0
-        else:
+    for i in range(num_demos):
+        for j in range(i+1,num_demos):
+            print(i,j)
+            traj_i = demonstrations[i]
+            traj_j = demonstrations[j]
             label = 1
+            training_obs.append((traj_i, traj_j))
+            training_labels.append(label)
 
-        training_obs.append((traj_i, traj_j))
-        training_labels.append(label)
-        max_traj_length = max(max_traj_length, len(traj_i), len(traj_j))
-
-
-    #fixed size snippets with progress prior
-    for n in range(num_snippets):
-        ti = 0
-        tj = 0
-        #only add trajectories that are different returns
-        while(ti == tj):
-            #pick two random demonstrations
-            ti = np.random.randint(num_demos)
-            tj = np.random.randint(num_demos)
-        #create random snippets
-        #find min length of both demos to ensure we can pick a demo no earlier than that chosen in worse preferred demo
-        min_length = min(len(demonstrations[ti]), len(demonstrations[tj]))
-        rand_length = np.random.randint(min_snippet_length, max_snippet_length)
-        if ti < tj: #pick tj snippet to be later than ti
-            ti_start = np.random.randint(min_length - rand_length + 1)
-            #print(ti_start, len(demonstrations[tj]))
-            tj_start = np.random.randint(ti_start, len(demonstrations[tj]) - rand_length + 1)
-        else: #ti is better so pick later snippet in ti
-            tj_start = np.random.randint(min_length - rand_length + 1)
-            #print(tj_start, len(demonstrations[ti]))
-            ti_start = np.random.randint(tj_start, len(demonstrations[ti]) - rand_length + 1)
-        traj_i = demonstrations[ti][ti_start:ti_start+rand_length:2] #skip everyother framestack to reduce size
-        traj_j = demonstrations[tj][tj_start:tj_start+rand_length:2]
-
-        max_traj_length = max(max_traj_length, len(traj_i), len(traj_j))
-        if ti > tj:
-            label = 0
-        else:
-            label = 1
-        training_obs.append((traj_i, traj_j))
-        training_labels.append(label)
-
-    print("maximum traj length", max_traj_length)
     return training_obs, training_labels
+
+
 
 
 
@@ -179,100 +124,34 @@ class Net(nn.Module):
         '''calculate cumulative return of trajectory'''
         sum_rewards = 0
         sum_abs_rewards = 0
-        x = traj.permute(0,3,1,2) #get into NCHW format
-        #compute forward pass of reward network (we parallelize across frames so batch size is length of partial trajectory)
-        x = F.leaky_relu(self.conv1(x))
-        x = F.leaky_relu(self.conv2(x))
-        x = F.leaky_relu(self.conv3(x))
-        x = F.leaky_relu(self.conv4(x))
-        x = x.view(-1, 784)
-        x = F.leaky_relu(self.fc1(x))
-        r = self.fc2(x)
-        sum_rewards += torch.sum(r)
-        sum_abs_rewards += torch.sum(torch.abs(r))
-        return sum_rewards, sum_abs_rewards
+        for x in traj:
+            x = x.permute(0,3,1,2) #get into NCHW format
+            #compute forward pass of reward network
+            x = F.leaky_relu(self.conv1(x))
+            x = F.leaky_relu(self.conv2(x))
+            x = F.leaky_relu(self.conv3(x))
+            x = F.leaky_relu(self.conv4(x))
+            x = x.view(-1, 784)
+            #x = x.view(-1, 1936)
+            x = F.leaky_relu(self.fc1(x))
+            #r = torch.tanh(self.fc2(x)) #clip reward?
+            r = self.fc2(x)
+            #r = torch.sigmoid(r) #TODO: try without this
+            sum_rewards += r
+        ##    y = self.scalar(torch.ones(1))
+        ##    sum_rewards += y
+        #print(sum_rewards)
+        return sum_rewards
 
 
 
     def forward(self, traj_i, traj_j):
         '''compute cumulative return for each trajectory and return logits'''
-        cum_r_i, abs_r_i = self.cum_return(traj_i)
-        cum_r_j, abs_r_j = self.cum_return(traj_j)
-        return torch.cat((cum_r_i.unsqueeze(0), cum_r_j.unsqueeze(0)),0), abs_r_i + abs_r_j
-
-
-
-
-# Train the network
-def learn_reward(reward_network, optimizer, training_inputs, training_outputs, num_iter, l1_reg, checkpoint_dir):
-    #check if gpu available
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    # Assume that we are on a CUDA machine, then this should print a CUDA device:
-    print(device)
-    loss_criterion = nn.CrossEntropyLoss()
-
-    cum_loss = 0.0
-    training_data = list(zip(training_inputs, training_outputs))
-    for epoch in range(num_iter):
-        np.random.shuffle(training_data)
-        training_obs, training_labels = zip(*training_data)
-        for i in range(len(training_labels)):
-            traj_i, traj_j = training_obs[i]
-            labels = np.array([training_labels[i]])
-            traj_i = np.array(traj_i)
-            traj_j = np.array(traj_j)
-            traj_i = torch.from_numpy(traj_i).float().to(device)
-            traj_j = torch.from_numpy(traj_j).float().to(device)
-            labels = torch.from_numpy(labels).to(device)
-
-            #zero out gradient
-            optimizer.zero_grad()
-
-            #forward + backward + optimize
-            outputs, abs_rewards = reward_network.forward(traj_i, traj_j)
-            outputs = outputs.unsqueeze(0)
-            loss = loss_criterion(outputs, labels) + l1_reg * abs_rewards
-            loss.backward()
-            optimizer.step()
-
-            #print stats to see if learning
-            item_loss = loss.item()
-            cum_loss += item_loss
-            if i % 100 == 99:
-                #print(i)
-                print("epoch {}:{} loss {}".format(epoch,i, cum_loss))
-                print(abs_rewards)
-                cum_loss = 0.0
-                print("check pointing")
-                torch.save(reward_net.state_dict(), checkpoint_dir)
-    print("finished training")
-
-
-
-
-
-def calc_accuracy(reward_network, training_inputs, training_outputs):
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    loss_criterion = nn.CrossEntropyLoss()
-    num_correct = 0.
-    with torch.no_grad():
-        for i in range(len(training_inputs)):
-            label = training_outputs[i]
-            traj_i, traj_j = training_inputs[i]
-            traj_i = np.array(traj_i)
-            traj_j = np.array(traj_j)
-            traj_i = torch.from_numpy(traj_i).float().to(device)
-            traj_j = torch.from_numpy(traj_j).float().to(device)
-
-            #forward to get logits
-            outputs, abs_return = reward_network.forward(traj_i, traj_j)
-            _, pred_label = torch.max(outputs,0)
-            if pred_label.item() == label:
-                num_correct += 1.
-    return num_correct / len(training_inputs)
-
-
-
+        #print([self.cum_return(traj_i), self.cum_return(traj_j)])
+        cum_r_i = self.cum_return(traj_i)
+        cum_r_j = self.cum_return(traj_j)
+        #print(abs_r_i + abs_r_j)
+        return torch.cat([cum_r_i, cum_r_j])
 
 
 
@@ -288,6 +167,60 @@ def predict_reward_sequence(net, traj):
 def predict_traj_return(net, traj):
     return sum(predict_reward_sequence(net, traj))
 
+def print_traj_returns(reward_net, demonstrations):
+    #print out predicted cumulative returns and actual returns
+    with torch.no_grad():
+        pred_returns = [predict_traj_return(reward_net, traj) for traj in demonstrations]
+    for i, p in enumerate(pred_returns):
+        print(i,p,sorted_returns[i])
+
+def calc_pairwise_ranking_loss(reward_net, demo_pairs, preference_labels):
+    '''sum over all pairwise demonstrations the softmax loss from T-REX and Christiano work'''
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # Assume that we are on a CUDA machine, then this should print a CUDA device:
+    print(device)
+    #don't need any gradients
+    with torch.no_grad():
+
+        loss_criterion = nn.CrossEntropyLoss()
+        cum_log_likelihood = 0.0
+        for i in range(len(preference_labels)):
+            traj_i, traj_j = demo_pairs[i]
+            labels = np.array([[training_labels[i]]])
+            traj_i = np.array(traj_i)
+            traj_j = np.array(traj_j)
+            traj_i = torch.from_numpy(traj_i).float().to(device)
+            traj_j = torch.from_numpy(traj_j).float().to(device)
+            labels = torch.from_numpy(labels).to(device)
+
+            #just need forward pass and loss calculation
+            outputs = reward_net.forward(traj_i, traj_j)
+            outputs = outputs.unsqueeze(0)
+            #print(outputs)
+            #print(labels)
+            log_likelihood = -loss_criterion(outputs, labels)
+            cum_log_likelihood += log_likelihood
+
+    return cum_log_likelihood
+
+
+def random_search(reward_net, demonstrations, num_trials, stdev = 0.1):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    '''try out a bunch of random weights and see what the resulting predicted returns look like'''
+
+    #create the pairwise rankings for loss calculations
+    demo_pairs, preference_labels = create_training_data(demonstrations)
+    for i in range(num_trials):
+        print()
+        print("trial", i)
+        #add random noise to weights
+        with torch.no_grad():
+            for param in reward_net.parameters():
+                param.add_(torch.randn(param.size()).to(device) * stdev)
+        print_traj_returns(reward_net, demonstrations)
+        cum_loss = calc_pairwise_ranking_loss(reward_net, demo_pairs, preference_labels)
+        print("pair-wise ranking loss", cum_loss)
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description=None)
@@ -319,16 +252,6 @@ if __name__=="__main__":
     np.random.seed(seed)
     tf.set_random_seed(seed)
 
-    print("Training reward for", env_id)
-    num_trajs =  args.num_trajs
-    num_snippets = args.num_snippets
-    min_snippet_length = 50 #min length of trajectory for training comparison
-    maximum_snippet_length = 100
-
-    lr = 0.00005
-    weight_decay = 0.0
-    num_iter = 5 #num times through training data
-    l1_reg=0.0
     stochastic = True
 
     env = make_vec_env(env_id, 'atari', 1, seed,
@@ -345,37 +268,25 @@ if __name__=="__main__":
 
     #sort the demonstrations according to ground truth reward to simulate ranked demos
 
-    demo_lengths = [len(d) for d in demonstrations]
-    print("demo lengths", demo_lengths)
-    max_snippet_length = min(np.min(demo_lengths), maximum_snippet_length)
-    print("max snippet length", max_snippet_length)
-
-    print(len(learning_returns))
-    print(len(demonstrations))
     print([a[0] for a in zip(learning_returns, demonstrations)])
     demonstrations = [x for _, x in sorted(zip(learning_returns,demonstrations), key=lambda pair: pair[0])]
 
     sorted_returns = sorted(learning_returns)
     print(sorted_returns)
 
-    training_obs, training_labels = create_training_data(demonstrations, num_trajs, num_snippets, min_snippet_length, max_snippet_length)
-    print("num training_obs", len(training_obs))
-    print("num_labels", len(training_labels))
+    #create the pairwise rankings for loss calculations
+    training_obs, training_labels = create_training_data(demonstrations)
 
     # Now we create a reward network and optimize it using the training data.
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     reward_net = Net()
     reward_net.to(device)
-    import torch.optim as optim
-    optimizer = optim.Adam(reward_net.parameters(),  lr=lr, weight_decay=weight_decay)
-    learn_reward(reward_net, optimizer, training_obs, training_labels, num_iter, l1_reg, args.reward_model_path)
+
+    #run random search over weights
+    random_search(reward_net, demonstrations, 10, stdev = 0.1)
     #save reward network
-    torch.save(reward_net.state_dict(), args.reward_model_path)
+    #torch.save(reward_net.state_dict(), args.reward_model_path)
 
-    #print out predicted cumulative returns and actual returns
-    with torch.no_grad():
-        pred_returns = [predict_traj_return(reward_net, traj) for traj in demonstrations]
-    for i, p in enumerate(pred_returns):
-        print(i,p,sorted_returns[i])
 
-    print("accuracy", calc_accuracy(reward_net, training_obs, training_labels))
+
+    #add random
