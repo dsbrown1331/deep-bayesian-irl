eeac861650c63620df498ef0381b63576432daa7
Daniel Brown <dsbrown1331@gmail.com>
2019-11-21 12:04:26 -0600

    ground truth policy eval

--- code/compute_fcounts_enduro.sh
+++ code/compute_fcounts_enduro.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for value in 00025 00325 00800 01450 mean map
+for value in 03125 03425 03900 04875 map mean
 do	
     python computePolicyExpectedFeatureCounts_gt.py --env_name $1 --num_rollouts 30 --output_id $value
 done
