7a0ec88faf6031edc2e30be90c40897dc57b8a93
Daniel Brown <dsbrown1331@gmail.com>
2020-01-23 09:20:37 -0600

    updated bash script

--- code/scripts/experiments/compute_fcounts_auxfeatures_64dim_6000train.sh
+++ code/scripts/experiments/compute_fcounts_auxfeatures_64dim_6000train.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-if [$1 = enduro]
+if [ $1 = "enduro" ]
 then
+  echo "computing fcounts for enduro"
   for value in 03125 03425 03900 04875 map mean
   do
       python ../../computePolicyExpectedFeatureCountsNetwork.py --env_name $1 --num_rollouts 100 --output_id $value --pretrained_network_dir /scratch/cluster/dsbrown/pretrained_networks/auxloss/ --postfix  _norecon_64.params_stripped.params --encoding_dims 64 --fcount_dir ~/Code/deep-bayesian-irl/policies/
