251fed1f290000882b447a3b34d4309f7d314b1f
dsbrown1331 <dsbrown1331@gmail.com>
2020-01-23 09:24:13 -0600

    bash scripts for other 12k and 128dim

---
+++ code/scripts/experiments/compute_fcounts_auxfeatures_64dim_12000train.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ $1 = "enduro" ]
+then
+  echo "computing fcounts for enduro"
+  for value in 03125 03425 03900 04875 map mean
+  do
+      python ../../computePolicyExpectedFeatureCountsNetwork.py --env_name $1 --num_rollouts 100 --output_id $value --pretrained_network_dir /scratch/cluster/dsbrown/pretrained_networks/auxloss/ --postfix  _norecon_64_12k.params_stripped.params --encoding_dims 64 --fcount_dir ~/Code/deep-bayesian-irl/policies/
+  done
+else
+  for value in 00025 00325 00800 01450 mean map
+  do
+      python ../../computePolicyExpectedFeatureCountsNetwork.py --env_name $1 --num_rollouts 100 --output_id $value --pretrained_network_dir /scratch/cluster/dsbrown/pretrained_networks/auxloss/ --postfix  _norecon_64_12k.params_stripped.params --encoding_dims 64 --fcount_dir ~/Code/deep-bayesian-irl/policies/
+  done
+fi
