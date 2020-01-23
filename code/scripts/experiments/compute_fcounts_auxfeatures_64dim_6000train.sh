c4faab840ae4646d44691a1b8851051b48cfe6ce
dsbrown1331 <dsbrown1331@gmail.com>
2020-01-22 21:06:13 -0600

    script for auxloss

--- code/scripts/experiments/compute_fcounts_auxfeatures_64dim_6000train.sh
+++ code/scripts/experiments/compute_fcounts_auxfeatures_64dim_6000train.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-for value in 03125 #03425 03900 04875 map mean
-do
+if [$1 = enduro]
+then
+  for value in 03125 03425 03900 04875 map mean
+  do
+      python ../../computePolicyExpectedFeatureCountsNetwork.py --env_name $1 --num_rollouts 100 --output_id $value --pretrained_network_dir /scratch/cluster/dsbrown/pretrained_networks/auxloss/ --postfix  _norecon_64.params_stripped.params --encoding_dims 64
+  done
+else
+  for value in 00025 00325 00800 01450 mean map
+  do
     python ../../computePolicyExpectedFeatureCountsNetwork.py --env_name $1 --num_rollouts 100 --output_id $value --pretrained_network_dir /scratch/cluster/dsbrown/pretrained_networks/auxloss/ --postfix  _norecon_64.params_stripped.params --encoding_dims 64
-done
+  done
+fi
