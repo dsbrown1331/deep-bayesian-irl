0399bd2f7eaffe500289b815838597c27d9f9b92
dsbrown1331 <dsbrown1331@gmail.com>
2020-01-29 11:06:18 -0600

    debugging bias

--- code/LinearFeatureMCMC_auxiliary.py
+++ code/LinearFeatureMCMC_auxiliary.py
@@ -594,6 +594,7 @@ if __name__=="__main__":
     best_reward_lastlayer = mcmc_map_search(reward_net, demonstrations, pairwise_prefs, demo_cnts, args.num_mcmc_steps, args.mcmc_step_size, args.weight_outputfile, args.weight_init)
     #turn this into a full network
     best_reward = EmbeddingNet(args.encoding_dims)
+    best_reward.fc2 = nn.Linear(num_features, 1, bias=False)
     best_reward.load_state_dict(torch.load(args.pretrained_network, map_location=device))
     best_reward.fc2 = best_reward_lastlayer
     best_reward.to(device)
