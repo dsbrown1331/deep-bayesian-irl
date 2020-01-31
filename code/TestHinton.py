c9ae695ade1793fe5605cd906448b52c93c1b4dd
dsbrown1331 <dsbrown1331@gmail.com>
2020-01-31 12:03:35 -0600

    testing imports

--- code/TestHinton.py
+++ code/TestHinton.py
@@ -24,11 +24,51 @@ import os
 
 
 
-def generate_novice_demos(env, env_name, agent, model_path ):
-    agent.load(model_path)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=None)
+    parser.add_argument('--env_id', default='', help='Select the environment to run')
+    parser.add_argument('--env_type', default='', help='mujoco or atari')
+    parser.add_argument('--model_path', default='')
+    parser.add_argument('--episode_count', default=100)
+    parser.add_argument('--record_video', action='store_true')
+    parser.add_argument('--render', action='store_true')
+
+    args = parser.parse_args()
+
+    stochastic = True #it helps Atari policies to not get stuck if there is a little noise
+
+    # You can set the level to logger.DEBUG or logger.WARN if you
+    # want to change the amount of output.
+    logger.set_level(logger.INFO)
+
+    #env = gym.make(args.env_id)
+
+    #env id, env type, num envs, and seed
+    env = make_vec_env(args.env_id, args.env_type, 1, 0,
+                       wrapper_kwargs={
+                           'clip_rewards':False,
+                           'episode_life':False,
+                       })
+    if args.record_video:
+        env = VecVideoRecorder(env,'./videos/',lambda steps: True, 200000) # Always record every episode
+
+    if args.env_type == 'atari':
+        env = VecFrameStack(env, 4)
+    elif args.env_type == 'mujoco':
+        env = VecNormalize(env,ob=True,ret=False,eval=True)
+    else:
+        assert False, 'not supported env type'
+
+    try:
+        env.load(args.model_path) # Reload running mean & rewards if available
+    except AttributeError:
+        pass
+
+    agent = PPO2Agent(env,args.env_type, stochastic)
+    agent.load(args.model_path)
     #agent = RandomAgent(env.action_space)
 
-    episode_count = 2
+    episode_count = args.episode_count
     reward = 0
     done = False
 
@@ -55,123 +95,3 @@ def generate_novice_demos(env, env_name, agent, model_path ):
 
     env.close()
     env.venv.close()
-
-
-    checkpoint_min = 50
-    checkpoint_max = 600
-    checkpoint_step = 50
-    checkpoints = []
-    if env_name == "enduro":
-        checkpoint_min = 3100
-        checkpoint_max = 3650
-    elif env_name == "seaquest":
-        checkpoint_min = 10
-        checkpoint_max = 65
-        checkpoint_step = 5
-    for i in range(checkpoint_min, checkpoint_max + checkpoint_step, checkpoint_step):
-        if i < 10:
-            checkpoints.append('0000' + str(i))
-        elif i < 100:
-            checkpoints.append('000' + str(i))
-        elif i < 1000:
-            checkpoints.append('00' + str(i))
-        elif i < 10000:
-            checkpoints.append('0' + str(i))
-    print(checkpoints)
-
-
-
-    demonstrations = []
-    learning_returns = []
-    learning_rewards = []
-    for checkpoint in checkpoints:
-
-        model_path = model_dir + "/models/" + env_name + "_25/" + checkpoint
-        if env_name == "seaquest":
-            model_path = model_dir + "/models/" + env_name + "_5/" + checkpoint
-
-        agent.load(model_path)
-        episode_count = 1
-        for i in range(episode_count):
-            done = False
-            traj = []
-            actions = []
-            gt_rewards = []
-            r = 0
-
-            ob = env.reset()
-            steps = 0
-            acc_reward = 0
-            #os.mkdir('images/' + str(checkpoint))
-            frameno = 0
-            while True:
-                action = agent.act(ob, r, done)
-                ob, r, done, info = env.step(action)
-                ob_processed = preprocess(ob, env_name)
-                ob_processed = ob_processed[0] #get rid of first dimension ob.shape = (1,84,84,4)
-                traj.append(ob_processed)
-                actions.append(action[0])
-                #save_image(torch.from_numpy(ob_processed).permute(2, 0, 1).reshape(4*84, 84), 'images/' + str(checkpoint) + '/' + str(frameno) + '_action_' + str(action[0]) + '.png')
-                frameno += 1
-
-                gt_rewards.append(r[0])
-                steps += 1
-                acc_reward += r[0]
-                if done:
-                    print("checkpoint: {}, steps: {}, return: {}".format(checkpoint, steps,acc_reward))
-                    break
-            print("traj length", len(traj))
-            print("demo length", len(demonstrations))
-            demonstrations.append([traj, actions])
-            learning_returns.append(acc_reward)
-            learning_rewards.append(gt_rewards)
-
-    return demonstrations, learning_returns, learning_rewards
-
-
-
-
-
-
-if __name__=="__main__":
-    parser = argparse.ArgumentParser(description=None)
-    parser.add_argument('--env_name', default='', help='Select the environment name to run, i.e. pong')
-    parser.add_argument('--models_dir', default = ".", help="path to directory that contains a models directory in which the checkpoint models for demos are stored")
-
-    args = parser.parse_args()
-    env_name = args.env_name
-    if env_name == "spaceinvaders":
-        env_id = "SpaceInvadersNoFrameskip-v4"
-    elif env_name == "mspacman":
-        env_id = "MsPacmanNoFrameskip-v4"
-    elif env_name == "videopinball":
-        env_id = "VideoPinballNoFrameskip-v4"
-    elif env_name == "beamrider":
-        env_id = "BeamRiderNoFrameskip-v4"
-    else:
-        env_id = env_name[0].upper() + env_name[1:] + "NoFrameskip-v4"
-
-    env_type = "atari"
-    print(env_type)
-    #set seeds
-    seed = 0
-    seed = int(seed)
-    torch.manual_seed(seed)
-    np.random.seed(seed)
-    tf.set_random_seed(seed)
-
-    stochastic = True
-
-    env = make_vec_env(env_id, 'atari', 1, seed,
-                       wrapper_kwargs={
-                           'clip_rewards':False,
-                           'episode_life':False,
-                       })
-
-    ACTION_DIMS = env.action_space.n
-    print("Number of actions", ACTION_DIMS)
-
-    env = VecFrameStack(env, 4)
-    agent = PPO2Agent(env, env_type, stochastic)
-
-    demonstrations, learning_returns, learning_rewards = generate_novice_demos(env, env_name, agent, args.models_dir)
