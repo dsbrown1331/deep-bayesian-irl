b2a354c3fd4383c25c23398da7161344ba591714
dsbrown1331 <dsbrown1331@gmail.com>
2020-02-03 15:28:26 -0600

    code for testing ss vs trex vs trex+ss

--- code/scripts/analyze_return_distribution_aux.py
+++ code/scripts/analyze_return_distribution_aux.py
@@ -20,13 +20,17 @@ print("alpha:", args.alpha)
 
 print(args.env_name)
 #read in the weights as a 2-d array and the feature counts of the policy
-W = helper.get_weightchain_array(args.mcmc_file, burn=2000, skip=50)
+W, likelihood = helper.get_weightchain_array(args.mcmc_file, burn=5000, skip=20, return_likelihood=True, preburn_until_accept=True)
+print("make sure that I actually accepted a value")
+print(np.sum(likelihood == -float('inf')))
+
+W = W[likelihood != -float('inf')]
 #print(np.mean(W, axis=0))
-eval_policies = ['00025', '00325', '00800', '01450', 'mean', 'map']
-name_transform = {'00025':'policy A', '00325':'policy B', '00800':'policy C', '01450':'policy D', 'mean':'mean', 'map':'MAP', 'noop': 'no-op'}
+eval_policies = ['00025', '00325', '00800', '01450', 'mean', 'map', 'no-op']
+name_transform = {'00025':'policy A', '00325':'policy B', '00800':'policy C', '01450':'policy D', 'mean':'mean', 'map':'MAP', 'no-op': 'no-op'}
 if args.env_name == "enduro":
-    eval_policies =['03125', '03425', '03900', '04875', 'mean', 'map']
-    name_transform = {'03125':'policy A', '03425':'policy B', '03900':'policy C', '04875':'policy D', 'mean':'mean', 'map':'MAP', 'noop': 'no-op'}
+    eval_policies =['03125', '03425', '03900', '04875', 'mean', 'map', 'no-op']
+    name_transform = {'03125':'policy A', '03425':'policy B', '03900':'policy C', '04875':'policy D', 'mean':'mean', 'map':'MAP', 'no-op': 'no-op'}
 gt_return_list = []
 fcount_list = []
 return_dist_list = []
@@ -34,24 +38,33 @@ print(" policy & mean & " + str(args.alpha) + "-VaR & mu+10*Var & ave length & g
 for eval in eval_policies:
     #print("-"*20)
     #print("eval", eval)
-    returns, fcounts = helper.parse_avefcount_array('../../policies/' + args.env_name +'_' + eval + args.identifier + '.params_stripped.params_fcounts_auxiliary.txt')
+    if eval is not "no-op":
+        fcounts, returns, lengths, all_fcounts = helper.parse_fcount_policy_eval('../../policies/' + args.env_name +'_' + eval + args.identifier + '.params_stripped.params_fcounts_auxiliary.txt')
+        return_dist = np.dot(W,fcounts)
+        #print(return_dist[:200])
+        #input()
+        print("{} & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.0f}  \\\\".format(name_transform[eval], np.mean(return_dist), helper.worst_percentile(return_dist, args.alpha), 10*helper.worst_percentile(return_dist, args.alpha) + np.mean(return_dist), np.mean(lengths), np.mean(returns), np.min(returns)))
+    else:
+        returns, fcounts = helper.parse_avefcount_array_noop('../../policies/' + args.env_name +'_' + eval + args.identifier + '.params_stripped.params_fcounts_auxiliary.txt')
+        return_dist = np.dot(W,fcounts)
+        print("{} & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.0f}  \\\\".format(name_transform[eval], np.mean(return_dist), helper.worst_percentile(return_dist, args.alpha), 10*helper.worst_percentile(return_dist, args.alpha) + np.mean(return_dist), 0, np.mean(returns), np.min(returns)))
     #print("num rollouts", len(returns))
-    return_dist = np.dot(W,fcounts)
 
-    print("{} & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.0f}  \\\\".format(name_transform[eval], np.mean(return_dist), helper.worst_percentile(return_dist, args.alpha), 10*helper.worst_percentile(return_dist, args.alpha) + np.mean(return_dist), 0, np.mean(returns), np.min(returns)))
+
+
 
     gt_return_list.append(returns)
     fcount_list.append(fcounts)
     return_dist_list.append(return_dist)
 
-if args.env_name == "breakout" and args.noop:
-    #evaluate the no-op policy
-    returns, fcounts = helper.parse_avefcount_array('../../policies/breakout_no-op' + args.identifier + '.params_stripped.params_fcounts_auxiliary.txt')
-    noop_returns = returns
-    #normalize
-    return_dist = np.dot(W,fcounts)
-    noop_return_dist = return_dist
-    print("{} & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.0f} \\\\".format("no-op", np.mean(return_dist), helper.worst_percentile(return_dist, args.alpha), 10*helper.worst_percentile(return_dist, args.alpha) + np.mean(return_dist), 0, np.mean(returns), np.min(returns)))
+# if args.env_name == "breakout" and args.noop:
+#     #evaluate the no-op policy
+#     returns, fcounts = helper.parse_avefcount_array('../../policies/breakout_no-op' + args.identifier + '.params_stripped.params_fcounts_auxiliary.txt')
+#     noop_returns = returns
+#     #normalize
+#     return_dist = np.dot(W,fcounts)
+#     noop_return_dist = return_dist
+#     print("{} & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.0f} \\\\".format("no-op", np.mean(return_dist), helper.worst_percentile(return_dist, args.alpha), 10*helper.worst_percentile(return_dist, args.alpha) + np.mean(return_dist), 0, np.mean(returns), np.min(returns)))
 
 if args.plot:
 
@@ -71,17 +84,25 @@ if args.plot:
 
     plt.figure()
     #plt.hist([return_dist_list[-2], return_dist_list[-1], noop_return_dist],30, label=["Mean", "MAP","noop])
-    plt.hist([return_dist_list[-2], return_dist_list[-1]],30, label=["Mean", "MAP"])
+    plt.hist([return_dist_list[eval_policies.index('mean')], return_dist_list[eval_policies.index('map')]],30, label=["Mean", "MAP"])
     plt.xlabel("Returns")
     plt.ylabel("Frequency")
     plt.title("predicted")
     plt.legend()
 
     plt.figure()
-    plt.hist([gt_return_list[-2], gt_return_list[-1]],30, label=["Mean", "MAP"  ])
+    plt.hist([gt_return_list[eval_policies.index('mean')], gt_return_list[eval_policies.index('map')]],30, label=["Mean", "MAP"  ])
     plt.xlabel("Returns")
     plt.ylabel("Frequency")
     plt.title("actual")
+    plt.legend()
+
+    plt.figure()
+    #plt.hist([return_dist_list[-2], return_dist_list[-1], noop_return_dist],30, label=["Mean", "MAP","noop])
+    plt.hist([return_dist_list[eval_policies.index('no-op')], return_dist_list[eval_policies.index('map')]],30, label=["no-op", "MAP"])
+    plt.xlabel("Returns")
+    plt.ylabel("Frequency")
+    plt.title("predicted")
     plt.legend()
 
 
--- code/scripts/helper.py
+++ code/scripts/helper.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def get_weightchain_array(mcmc_chain_filename, burn=5000, skip=20, return_likelihood=False):
+def get_weightchain_array(mcmc_chain_filename, burn=5000, skip=20, return_likelihood=False, preburn_until_accept=True):
     #load the mean of the MCMC chain
     reader = open(mcmc_chain_filename)
     data = []
@@ -12,16 +12,40 @@ def get_weightchain_array(mcmc_chain_filename, burn=5000, skip=20, return_likeli
             np_line.append(float(s))
         likelihood.append(float(parsed[-1]))
         data.append(np_line)
-    data = np.array(data)
-    data = data[burn::skip,:]
     likelihood = np.array(likelihood)
+    data = np.array(data)
+    data = data[likelihood != -float('inf')]
+    print(data.shape)
+    num_preburn = np.sum(likelihood == -float('inf'))
+    likelihood = likelihood[likelihood != -float('inf')]
+    print('preburning ', num_preburn, "to find first accept")
+    data = data[burn::skip,:]
+    print(data.shape)
     likelihood = likelihood[burn::skip]
     print("chain shape", data.shape)
     reader.close()
+    assert(len(likelihood) == len(data))
     if not return_likelihood:
         return data
     else:
         return data, likelihood
+
+#deprecated...use newer version for parsing newer policy eval output files
+def parse_avefcount_array_noop(fcount_file):
+    '''returns a list of returns and a numpy array of ave feature counts'''
+    reader = open(fcount_file)
+    weights = []
+    returns = []
+    for i,line in enumerate(reader):
+        if i == 0: #read in the np_weights
+            parsed = line.strip().split(',')
+            for w in parsed:
+                weights.append(float(w))
+        elif i == 2: #read in the returns
+            parsed = line.strip().split(',')
+            for r in parsed:
+                returns.append(float(r))
+    return returns, weights
 
 def parse_fcount_policy_eval(fcount_file):
     '''returns a list of returns and a numpy array of ave feature counts'''
