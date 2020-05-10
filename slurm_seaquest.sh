7efaec8776c9aad323b8ee9fe62309355971e23c
dsbrown1331 <dsbrown1331@gmail.com>
2020-05-09 22:22:16 -0500

    seaquest slurm test

---
+++ slurm_seaquest.sh
@@ -0,0 +1,2 @@
+conda activate deeplearning
+OPENAI_LOG_FORMAT='stdout,log,csv,tensorboard' OPENAI_LOGDIR=/scratch/cluster/dsbrown/tflogs/a2c/seaquest_rl python -m baselines.run --alg=a2c --env=SeaquestNoFrameskip-v4 --seed 0 --num_timesteps=5e7  --save_interval=1000 --num_env 9
